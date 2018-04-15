f504ceffe9e239d8d294f810a71ad7194f8069a7
Rkatze <silence.chan21@gmail.com>
2018-04-15 22:37:24 +0200

    assignment project

---
+++ plot2.R
@@ -0,0 +1,17 @@
+setwd("/Users/silencemac/Desktop/coursera/course4.1")
+library(data.table)
+
+#missing values are coded as "?". this can be changed by "na.strings".
+dataOrig <- read.table("./household_power_consumption.txt", header=T, sep=";",
+                       na.strings = "?", stringsAsFactors=F)
+
+#subset dataset with two days only
+dataSub <- subset(dataOrig, Date %in% c("1/2/2007", "2/2/2007"))
+#convert dates and times
+dataSub$Date <- as.Date(dataSub$Date, "%d/%m/%Y")
+dateTime <- paste(dataSub$Date, dataSub$Time, sep = " ", "%Y-%m-%d %H:%M:%S")
+dataSub$dateTime <- as.POSIXct(dateTime)
+
+#type = "l" means lines; for the second plot
+plot(Global_active_power ~ dateTime, data = dataSub, type = "l", xlab = "", 
+     ylab = "Global Active Power (kilowatts)")
